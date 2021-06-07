8dab74fd59be7aae556cae6df3498cd7f7cbebcc
M-ELMOUSSAOUI <mosawi.man@live.fr>
2021-06-07 21:24:05 +0100

    firebase

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import BottomHomeNavigation from './app/Navigation/BottomNavigation/MainBottomNavigation/MainBottomNavigation'
+import SignIn from './app/OnBording/SignIn/SignIn'
 export default function App() {
   return (
-    <BottomHomeNavigation />
+    
+     <BottomHomeNavigation />
   );
 }
 
