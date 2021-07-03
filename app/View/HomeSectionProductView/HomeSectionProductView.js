65d75c436e8d0adfb2097d54607243956531eb9c
moad <moad.zizi@hotmail.com>
2021-07-03 12:10:48 +0100

    Adding Product Details

--- app/View/HomeSectionProductView/HomeSectionProductView.js
+++ app/View/HomeSectionProductView/HomeSectionProductView.js
@@ -4,16 +4,16 @@ import { View, Text } from "react-native";
 import Product from "../../Components/Product/Product";
 import styles from "./HomeSectionProductView.style";
 import HeaderCategories from '../../Components/HeaderCategories'
-export default function HomeSectionProductView() {
+export default function HomeSectionProductView({navigation}) {
   return (
     <View>
       <ScrollView style={styles.container} showsVerticalScrollIndicator={false}>
       <HeaderCategories />
 
-        <Product numberImages={3} location="Tanger, Val Fleuri" nbLikes={55} title="Hundai i30" price={10000} />
-        <Product numberImages={1} location="Casablanca, El bernousi" nbLikes={10} title="Apparetement par jour" price={400}/>
-        <Product numberImages={2} location="Merakech, Gulize" nbLikes={0} title="Iphone 13 new lbi3" price={5708}/>
-        <Product numberImages={5} location="Agadir, Hay saada" nbLikes={2} title="Plombie Agadir safae" price={4000}/>
+        <Product numberImages={3} location="Tanger, Val Fleuri" nbLikes={55} title="Hundai i30" price={10000} navigation={navigation} />
+        <Product numberImages={1} location="Casablanca, El bernousi" nbLikes={10} title="Apparetement par jour" price={400} navigation={navigation} />
+        <Product numberImages={2} location="Merakech, Gulize" nbLikes={0} title="Iphone 13 new lbi3" price={5708} navigation={navigation} />
+        <Product numberImages={5} location="Agadir, Hay saada" nbLikes={2} title="Plombie Agadir safae" price={4000} navigation={navigation} />
       </ScrollView>
     </View>
   );
