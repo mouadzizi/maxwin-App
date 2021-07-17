926b56f749fe5de9a3bed5cf46fcdf4e70dec571
moad <moad.zizi@hotmail.com>
2021-07-17 15:18:28 +0100

    Version 1 of Favorit

---
+++ app/Components/Product/FavoritProduct/FavoritProduct.style.js
@@ -0,0 +1,48 @@
+import { StyleSheet } from "react-native";
+import { COLORS } from "../../../GlobalStyle";
+export default StyleSheet.create({
+  container: {
+    height: 130,
+    margin: 10,
+    flexDirection: "row",
+    borderRadius: 10,
+  },
+  containerImage: {
+    flex: 2,
+    borderColor: COLORS.secondary,
+    borderLeftWidth: 1,
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+    borderTopLeftRadius: 10,
+    borderBottomLeftRadius: 10,
+    justifyContent: "center",
+  },
+  containerInfo: {
+    flex: 3,
+    borderColor: COLORS.secondary,
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+    justifyContent: 'center'
+  },
+  containerBin: {
+    borderTopRightRadius: 10,
+    borderBottomRightRadius: 10,
+    borderColor: COLORS.secondary,
+    borderWidth: 1,
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  image: {
+    width: "90%",
+    height: "90%",
+    alignSelf: "center",
+    borderRadius: 10,
+    marginTop: 5,
+  },
+  info:{
+      fontSize: 15,
+      fontFamily: 'monospace',
+      color: COLORS.primary
+  }
+});
