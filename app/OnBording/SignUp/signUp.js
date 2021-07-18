f62b696d3054c34a8383fe0b059b9ea6e63d67fd
USERSAD\moelmous <moelmous@everis.com>
2021-07-18 12:29:51 +0100

    sign in added

--- app/OnBording/SignUp/signUp.js
+++ app/OnBording/SignUp/signUp.js
@@ -1,10 +1,55 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState } from "react";
+import { View, Text } from "react-native";
+import styles from "./signUp.style";
+import { Input } from "react-native-elements";
+import ButtonFill from "../../Components/Button/ButtonFill";
+import { createUser } from "../../API/APIFunctions";
+import {} from 'firebase/app'
 
-export default function signUp() {
-    return (
-        <View>
-            <Text></Text>
+export default function signUp({ navigation }) {
+  const [loading, setLoading] = useState(false);
+  const [user, setUser] = useState({});
+
+  const createNewUser = () => {
+      setLoading(true)
+    createUser(user)
+      .then(()=>{
+        navigation.goBack()
+        setLoading(false)
+      })
+      .catch(({ message }) =>{
+        alert(message);
+        setLoading(false)
+      });
+  };
+  return (
+    <View style={styles.container}>
+      <View style={styles.inputContainer}>
+        <View style={styles.input}>
+          <Input
+          keyboardType='email-address'
+            label="Email"
+            placeholder="Entrer votre e-mail"
+            autoCapitalize="none"
+            onChangeText={(e) => setUser({ ...user, email: e })}
+          />
         </View>
-    )
+
+        <View style={styles.input}>
+          <Input
+            placeholder="Password"
+            label="Entrer votre mot de passe"
+            secureTextEntry
+            onChangeText={(e) => setUser({ ...user, password: e })}
+          />
+        </View>
+        <ButtonFill
+          onClick={createNewUser}
+          loading={loading}
+          style={styles.button}
+          title="Sinscrire"
+        />
+      </View>
+    </View>
+  );
 }
