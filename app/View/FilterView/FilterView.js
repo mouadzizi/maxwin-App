ed0ee3df8410461f42b251c18040be6f65c92e43
moad <moad.zizi@hotmail.com>
2021-07-22 14:16:03 +0100

    start Filter

--- app/View/FilterView/FilterView.js
+++ app/View/FilterView/FilterView.js
@@ -1,11 +1,46 @@
+import { Picker } from "@react-native-picker/picker";
 import React from "react";
+import { ScrollView } from "react-native";
 import { View, Text } from "react-native";
+import { COLORS } from "../../GlobalStyle";
 import styles from "./FilterView.style";
-
+import ButtonOutlined from "../../Components/Button/ButtonOutlined"
 export default function FilterView() {
   return (
-    <View style={styles.container}>
-      <Text> this is the Filter Page</Text>
+    <View style={{marginTop : 20}}>
+      <ScrollView style={styles.container} showsVerticalScrollIndicator={false}>
+
+        <View style={styles.pickerView}>
+          <Text style={styles.label}>Choisissez la Ville</Text>
+          <Picker
+            style={styles.pickerInput}
+            mode="dialog"
+            dropdownIconColor={COLORS.primary}
+            selectedValue={""}
+            onValueChange={() => console.log("hey")}
+          >
+            <Picker.Item
+              label="Choisissez une Ville"
+              value=""
+              color={COLORS.Grey[400]}
+            />
+            <Picker.Item label="AL Hoceima" value="ALHoceima" />
+            <Picker.Item label="Agadir" value="Agadir" />
+            <Picker.Item label="Casablanca" value="Casablanca" />
+            <Picker.Item label="Dakhla" value="Dakhla" />
+            <Picker.Item label="Fès" value="Fès" />
+            <Picker.Item label="Kénitra" value="Kénitra" />
+            <Picker.Item label="Marrakech" value="Marrakech" />
+            <Picker.Item label="Meknès" value="Meknès" />
+            <Picker.Item label="Ouajda" value="Ouajda" />
+            <Picker.Item label="Rabat" value="Rabat" />
+            <Picker.Item label="Tanger" value="Tanger" />
+            <Picker.Item label="Tetouan" value="Tetouan" />
+          </Picker>
+        </View>
+
+        <ButtonOutlined title="Choisissez une categorie" onClick={()=> alert('modal for category')}/>
+      </ScrollView>
     </View>
   );
 }
--- app/View/FilterView/FilterView.style.js
+++ app/View/FilterView/FilterView.style.js
@@ -1,7 +1,23 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
+import { COLORS } from "../../GlobalStyle";
 export default StyleSheet.create({
-    container:{
-        flex: 1,
-        alignItems: 'center'
-    },
-})
+  container: {
+    paddingHorizontal: 20,
+  },
+  pickerView: {
+    borderBottomWidth: 1,
+    marginBottom: 20,
+    width: "100%",
+    borderColor: COLORS.Grey[300],
+  },
+  pickerInput: {
+    height: 50,
+    width: '100%'
+},
+label: {
+    color: COLORS.primary,
+    fontSize: 16,
+    marginLeft: 10,
+    fontWeight: 'bold'
+},
+});
