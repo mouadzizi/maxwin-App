c3d5ac7463d3dbb95f9f417b984278c84cab4d2f
moad <moad.zizi@hotmail.com>
2021-07-23 09:41:51 +0100

    MORE FILTER

--- app/View/FilterView/FilterView.js
+++ app/View/FilterView/FilterView.js
@@ -1,15 +1,17 @@
-import { Picker } from "@react-native-picker/picker";
 import React from "react";
-import { ScrollView } from "react-native";
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
+import { Picker } from "@react-native-picker/picker";
+import { Input } from "react-native-elements";
+import ButtonOutlined from "../../Components/Button/ButtonOutlined";
+import ButtonFill from "../../Components/Button/ButtonFill";
+
 import { COLORS } from "../../GlobalStyle";
 import styles from "./FilterView.style";
-import ButtonOutlined from "../../Components/Button/ButtonOutlined"
-export default function FilterView() {
-  return (
-    <View style={{marginTop : 20}}>
-      <ScrollView style={styles.container} showsVerticalScrollIndicator={false}>
 
+export default function FilterView({ navigation, route }) {
+  return (
+    <View style={{ marginTop: 20, flex: 1}}>
+      <ScrollView style={styles.container} showsVerticalScrollIndicator={false}>
         <View style={styles.pickerView}>
           <Text style={styles.label}>Choisissez la Ville</Text>
           <Picker
@@ -39,8 +41,108 @@ export default function FilterView() {
           </Picker>
         </View>
 
-        <ButtonOutlined title="Choisissez une categorie" onClick={()=> alert('modal for category')}/>
+        <ButtonOutlined
+          title="Choisissez une categorie"
+          onClick={() => alert("modal for category")}
+          style={{ marginBottom: 20 }}
+        />
+
+        <Text style={styles.label}>Prix</Text>
+
+        <View style={styles.row}>
+          <Input
+            keyboardType="numeric"
+            placeholder="Prix MIN"
+            containerStyle={{ width: "50%" }}
+            labelStyle={{ color: COLORS.primary }}
+          />
+          <Input
+            keyboardType="numeric"
+            placeholder="Prix MAX"
+            containerStyle={{ width: "50%" }}
+            labelStyle={{ color: COLORS.primary }}
+          />
+        </View>
+
+        <View style={styles.pickerView}>
+          <Text style={styles.label}>Marque de Voiture</Text>
+          <Picker
+            style={styles.pickerInput}
+            mode="dialog"
+            dropdownIconColor={COLORS.primary}
+          >
+            <Picker.Item
+              label="Choisissez une marque"
+              value=""
+              color={COLORS.Grey[400]}
+            />
+            <Picker.Item label="AUDI" value="AUDI" />
+            <Picker.Item label="BMW" value="BMW" />
+            <Picker.Item label="CHEVROLET" value="CHEVROLET" />
+          </Picker>
+        </View>
+
+        <Text style={styles.label}>Kilométrage</Text>
+
+        <View style={styles.row}>
+          <Input
+            keyboardType="numeric"
+            placeholder="MIN"
+            containerStyle={{ width: "50%" }}
+            labelStyle={{ color: COLORS.primary }}
+          />
+          <Input
+            keyboardType="numeric"
+            placeholder="MAX"
+            containerStyle={{ width: "50%" }}
+            labelStyle={{ color: COLORS.primary }}
+          />
+        </View>
+
+        <View style={styles.pickerView}>
+          <Text style={styles.label}>Carburant</Text>
+          <Picker
+            style={styles.pickerInput}
+            mode="dropdown"
+            dropdownIconColor={COLORS.primary}
+          >
+            <Picker.Item
+              label="Choisissez le carburant"
+              value=""
+              color={COLORS.Grey[400]}
+            />
+            <Picker.Item label="Diesel" value="Diesel" />
+            <Picker.Item label="Essence" value="Essence" />
+          </Picker>
+        </View>
+
+        <Input
+          label="Kilométrage"
+          placeholder="Merci d'entrer le Nom exact de votre article"
+          style={{ fontSize: 15 }}
+          labelStyle={{ color: COLORS.primary }}
+        />
+
+        <Input
+          label="Kilométrage"
+          placeholder="Merci d'entrer le Nom exact de votre article"
+          style={{ fontSize: 15 }}
+          labelStyle={{ color: COLORS.primary }}
+        />
+
+        <Input
+          label="Kilométrage"
+          placeholder="Merci d'entrer le Nom exact de votre article"
+          style={{ fontSize: 15 }}
+          labelStyle={{ color: COLORS.primary }}
+        />
       </ScrollView>
+
+      <ButtonFill
+        title="Validé"
+        style={{ marginHorizontal: 20, marginBottom: 20 }}
+        loading={false}
+      />
     </View>
   );
 }
--- app/View/ProfileView/ProfileView.js
+++ app/View/ProfileView/ProfileView.js
@@ -2,12 +2,13 @@ import React from "react";
 import { View, Text, Alert } from "react-native";
 import styles from "./ProfileView.style";
 import ProfileSection from "./ProfileSection";
-import {GlobalStyle} from '../../GlobalStyle'
+import Devider from "../../Components/Divider";
+
+import { GlobalStyle } from "../../GlobalStyle";
 import { auth } from "../../API/Firebase";
 export default function ProfileView() {
   return (
     <View style={styles.container}>
-
       <Text style={GlobalStyle.H1}>Profile</Text>
       <ProfileSection
         subTitle="Informations personnelles"
@@ -15,7 +16,7 @@ export default function ProfileView() {
         onClick={() => Alert.alert("Informations personnelles")}
       />
 
-      <Text style={[GlobalStyle.H1, {marginTop: 20}]}>Settings</Text>
+      <Text style={[GlobalStyle.H1, { marginTop: 20 }]}>Settings</Text>
       <ProfileSection
         title="Contacter Nous"
         subTitle="E-Mail"
@@ -31,8 +32,15 @@ export default function ProfileView() {
       <ProfileSection
         title="FAQ"
         subTitle="Questions fréquemment"
-        onClick={()=>auth.signOut()}
+        onClick={() => Alert.alert("Questions fréquemment Page")}
         iconName="FAQ"
+      />
+      <Devider width="100%" style={{marginVertical : 20}}/>
+      <ProfileSection
+        title="Se déconnecter"
+        subTitle="Se déconnecter"
+        onClick={() => auth.signOut()}
+        iconName="sa"
       />
     </View>
   );
