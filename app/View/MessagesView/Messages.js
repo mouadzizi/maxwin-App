3433dee0f794ea0d02d7c711a8229bb35f3a9189
USERSAD\moelmous <moelmous@everis.com>
2021-08-10 18:19:19 +0100

    chat added

--- app/View/MessagesView/Messages.js
+++ app/View/MessagesView/Messages.js
@@ -2,67 +2,95 @@ import React, { useCallback, useEffect, useState } from "react";
 import { View, Text } from "react-native";
 import { FlatList } from "react-native-gesture-handler";
 import EmptyChats from "../../SVG/EmptyChats";
-import Conversation from '../../Components/Conversation'
 import styles from "./MessagesView.style";
 import Conversation from "../../Components/Conversation/Conversation";
 import { db, auth } from "../../API/Firebase";
 
 export default function MessagesView({ navigation }) {
   const [conversation, setConversation] = useState([]);
-  const uid = auth.currentUser?.uid
+  const [user, setUser] = useState({});
   const chatRef = db.collection("chats");
 
-  const fetchConversations = useCallback((snapShot) => {
-    const conversations = snapShot.docs
-      .filter(
-        (doc) =>
-          doc.data().contact1._id.search(uid) >= 0 ||
-          doc.data().contact2._id.search(uid) >= 0
-      )
-      .map((d) => {
-        return {
-          key: d.id,
-          ...d.data(),
-        };
-      });
-    Promise.all(conversations).then((res) => setConversation(res));
-  }, []);
+  // const fetchConversations = useCallback((snapShot) => {
+  //   const conversations = snapShot.docs
+  //     .filter(
+  //       (doc) =>
+  //         doc.data().contact1._id.search(user.uid) >= 0 ||
+  //         doc.data().contact2._id.search(user.uid) >= 0
+  //     )
+  //     .map((d) => {
+  //       return {
+  //         key: d.id,
+  //         ...d.data(),
+  //       };
+  //     });
+  //   Promise.all(conversations).then((res) => setConversation(res));
+  // }, []);
 
   useEffect(() => {
-    console.log('fetching');
+    if (user) {
       var cleanup = chatRef
         .orderBy("createdAt", "desc")
-        .onSnapshot(fetchConversations);
-    
+        .onSnapshot((snapShot) => {
+          const conversations = snapShot.docs
+            .filter(
+              (doc) =>
+                doc.data().contact1._id.search(user.uid) >= 0 ||
+                doc.data().contact2._id.search(user.uid) >= 0
+            )
+            .map((d) => {
+              return {
+                key: d.id,
+                ...d.data(),
+              };
+            });
+          setConversation(conversations);
+        });
+    }
+
     return () => {
       cleanup;
       setConversation([]);
     };
-  }, [uid]);
+  }, [user]);
 
+  useEffect(() => {
+    const cleanUp = auth.onAuthStateChanged((user) => {
+      if (user) {
+        setUser(user);
+      } else console.log("user not exist");
+    });
+    return () => {
+      cleanUp();
+    };
+  }, []);
 
   const goToChat = (item) => {
-    db.collection('chats').doc(item.key).update({seen:true}).then(()=>{
-      navigation.navigate('ChatView',{
-        seller: (uid === item.contact1._id) ? item.contact2 : item.contact1,
-        chatId: item.key,
-        pic: item.chatPic,
-        postTitle: item.title,
-        postId:item.postId
-      })
-    })
+    db.collection("chats")
+      .doc(item.key)
+      .update({ seen: true })
+      .then(() => {
+        navigation.navigate("ChatView", {
+          seller:
+            user.uid === item.contact1._id ? item.contact2 : item.contact1,
+          chatId: item.key,
+          pic: item.chatPic,
+          postTitle: item.title,
+          postId: item.postId,
+        });
+      });
   };
 
   const renderItem = useCallback(
     ({ item }) => (
       <Conversation
-        seen={(uid === item.contact1._id) ? false : !item.seen}
+        seen={user.uid === item.contact1._id ? false : !item.seen}
         picture={item.chatPic}
         lastMessage={item.lastMessage}
         title={item.title}
-        onClick={()=>goToChat(item)}
+        onClick={() => goToChat(item)}
         // time={item.createdAt.toDate().toLocaleTimeString()}
-        contact={uid == item.contact1._id ? "Vous" : item.contact1.name}
+        contact={user.uid == item.contact1._id ? "Vous" : item.contact1.name}
       />
     ),
     []
